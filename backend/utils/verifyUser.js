b6ab0d8558c9d1b32b9ee0b5078976c04c4aa90d
ThakkashilaPadmavathi <156405697+ThakkashilaPadmavathi@users.noreply.github.com>
2025-07-20 22:57:40 +0530

    Commit 4

--- backend/utils/verifyUser.js
+++ backend/utils/verifyUser.js
@@ -5,12 +5,12 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
   if (!token) {
-    return next(errorHandler(401, "Not authenticated"));
+    return next(errorHandler(401, "Unauthorized: No token"));
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, "Invalid token"));
-    req.user = user;
+    req.user = user; // Important: Attach decoded token data
     next();
   });
 };
