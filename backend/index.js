44474147527d7e3399611a968025d03b540f3efe
ThakkashilaPadmavathi <156405697+ThakkashilaPadmavathi@users.noreply.github.com>
2025-07-20 21:48:53 +0530

    Updated vercel and render links

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import cors from "cors"
 dotenv.config()
 
 mongoose
-  .connect("mongodb://localhost:27017/noteapp")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to mongoDB")
   })
@@ -20,7 +20,7 @@ const app = express()
 // to make input as json
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
+app.use(cors({ origin: "https://mern-project-eight-tan.vercel.app/", credentials: true }))
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -36,7 +36,7 @@ const Signup = () => {
     // sign up api
     try {
       const res = await axios.post(
-        "http://localhost:3000/api/auth/signup",
+        "https://mern-project-gssm.onrender.com/api/auth/signup",
         { username: name, email, password },
         { withCredentials: true }
       )
